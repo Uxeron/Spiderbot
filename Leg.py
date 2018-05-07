657a88962586d97b058faf342eb5ea16aeffee26
Uxeron <vgabrielius123@gmail.com>
2018-05-07 22:29:34 +0300

    Shortened leg control function

--- Leg.py
+++ Leg.py
@@ -9,68 +9,32 @@ pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 motors = [8, 9, 10, 4, 5, 6]
-start = [170, 130, 180, 160, 130, 180]
+start = [170, 130, 210, 160, 130, 210]
 limits = [330, 360, 210, 330, 360, 230]
 pos = [100, 250, 0, 100, 250, 0]
 speeds = [0, 0, 0, 0, 0, 0]
 
 interval = 0.002
-speed = 2
+speed = 3
 divisor = 30
+                        # Digital inputs
+movements = {"BTN_TL":   "speeds[5] = -speed if state else 0",
+             "ABS_BRAKE":"speeds[5] =  speed if state else 0",
+             "BTN_TR":   "speeds[2] = -speed if state else 0",
+             "ABS_GAS":  "speeds[2] =  speed if state else 0",
+                        # Analog inputs
+             "ABS_X":    "speeds[3] = (state - 128)/divisor",
+             "ABS_Y":    "speeds[4] = (128 - state)/divisor",
+             "ABS_Z":    "speeds[0] = (state - 128)/divisor",
+             "ABS_RZ":   "speeds[1] = (128 - state)/divisor"}
 
 def readInput():
     while True:
         events = get_gamepad()
         for event in events:
-            if event.code == "BTN_TL":
-                if event.state == 1:
-                    speeds[5] = -speed
-                else:
-                    speeds[5] = 0;
-            elif event.code == "ABS_BRAKE":
-                if event.state == 255:
-                    speeds[5] = speed
-                else:
-                    speeds[5] = 0;
-            elif event.code == "BTN_TR":
-                if event.state == 1:
-                    speeds[2] = -speed
-                else:
-                    speeds[2] = 0
-            elif event.code == "ABS_GAS":
-                if event.state == 255:
-                    speeds[2] = speed
-                else:
-                    speeds[2] = 0
-            elif event.code == "ABS_X":
-                if event.state < 128:
-                    speeds[3] = -(128 - event.state)/divisor
-                elif event.state > 128:
-                    speeds[3] = (event.state - 128)/divisor
-                else:
-                    speeds[3] = 0
-            elif event.code == "ABS_Y":
-                if event.state < 128:
-                    speeds[4] = (128 - event.state)/divisor
-                elif event.state > 128:
-                    speeds[4] = -(event.state - 128)/divisor
-                else:
-                    speeds[4] = 0
-
-            elif event.code == "ABS_Z":
-                if event.state < 128:
-                    speeds[0] = -(128 - event.state)/divisor
-                elif event.state > 128:
-                    speeds[0] = (event.state - 128)/divisor
-                else:
-                    speeds[0] = 0
-            elif event.code == "ABS_RZ":
-                if event.state < 128:
-                    speeds[1] = (128 - event.state)/divisor
-                elif event.state > 128:
-                    speeds[1] = -(event.state - 128)/divisor
-                else:
-                    speeds[1] = 0
+            if event.code in movements.keys():
+                state = event.state
+                exec(movements[event.code])
 
 
 thread.start_new_thread(readInput, ())
@@ -84,9 +48,11 @@ try:
             try:
                 pwm.set_pwm(motors[i], 0, start[i] + int(round(pos[i])))
             except IOError:
+                print "IO Error occurred, continuing..."
                 continue
-        print pos
+        print map(int, pos)
         #print speeds
         sleep(interval)
 except KeyboardInterrupt:
     pwm = Adafruit_PCA9685.PCA9685()
+    print "Program stopping"
