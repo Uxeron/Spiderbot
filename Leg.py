f179387d6c76a4adb8bbed300f17dd60b19b2234
Uxeron <vgabrielius123@gmail.com>
2018-05-12 00:30:14 +0300

    Added 2nd servo controller, moved variables

--- Leg.py
+++ Leg.py
@@ -6,12 +6,14 @@ import sys
 from time import sleep
 from inputs import get_gamepad
 
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(50)
+pwmL = Adafruit_PCA9685.PCA9685(0x41)
+pwmR = Adafruit_PCA9685.PCA9685(0x40)
+pwmL.set_pwm_freq(50)
+pwmR.set_pwm_freq(50)
 
-motors = [8, 9, 10, 4, 5, 6]
-start = [170, 130, 210, 160, 130, 210]
-limits = [330, 360, 210, 330, 360, 230]
+motors = [12, 13, 14, 3, 2, 1]
+start = [160, 130, 210, 170, 130, 210]
+limits = [330, 360, 230, 330, 360, 210]
 pos = [100, 250, 0, 100, 250, 0]
 speeds = [0, 0, 0, 0, 0, 0]
 
@@ -21,15 +23,15 @@ divisor = 30
 
 exit = False
                         # Digital inputs
-movements = {"BTN_TL":   "speeds[5] = -speed if state else 0",
-             "ABS_BRAKE":"speeds[5] =  speed if state else 0",
-             "BTN_TR":   "speeds[2] = -speed if state else 0",
-             "ABS_GAS":  "speeds[2] =  speed if state else 0",
+movements = {"BTN_TL":   "speeds[2] = -speed if state else 0",
+             "ABS_BRAKE":"speeds[2] =  speed if state else 0",
+             "BTN_TR":   "speeds[5] = -speed if state else 0",
+             "ABS_GAS":  "speeds[5] =  speed if state else 0",
                         # Analog inputs
-             "ABS_X":    "speeds[3] = (state - 128)/divisor",
-             "ABS_Y":    "speeds[4] = (128 - state)/divisor",
-             "ABS_Z":    "speeds[0] = (state - 128)/divisor",
-             "ABS_RZ":   "speeds[1] = (128 - state)/divisor",
+             "ABS_X":    "speeds[0] = (state - 128)/divisor",
+             "ABS_Y":    "speeds[1] = (128 - state)/divisor",
+             "ABS_Z":    "speeds[3] = (state - 128)/divisor",
+             "ABS_RZ":   "speeds[4] = (128 - state)/divisor",
 
              "BTN_START":"global exit; exit = True"}
 
@@ -77,17 +79,28 @@ try:
     while True:
         if exit:
             raise KeyboardInterrupt
-        for i in xrange(6):
+
+        for i in xrange(3):
             if 0 < pos[i] + speeds[i] < limits[i]:
                 pos[i] += speeds[i]
             try:
-                pwm.set_pwm(motors[i], 0, start[i] + int(round(pos[i])))
+                pwmL.set_pwm(motors[i], 0, start[i] + int(round(pos[i])))
+            except IOError:
+                print "IO Error occurred, continuing..."
+                continue
+
+        for i in xrange(3, 6):
+            if 0 < pos[i] + speeds[i] < limits[i]:
+                pos[i] += speeds[i]
+            try:
+                pwmR.set_pwm(motors[i], 0, start[i] + int(round(pos[i])))
             except IOError:
                 print "IO Error occurred, continuing..."
                 continue
         print map(int, pos)
         sleep(interval)
 except KeyboardInterrupt:
-    pwm = Adafruit_PCA9685.PCA9685()
+    pwmL = Adafruit_PCA9685.PCA9685(0x41)
+    pwmR = Adafruit_PCA9685.PCA9685(0x40)
     writer()
     print "Program stopping"
